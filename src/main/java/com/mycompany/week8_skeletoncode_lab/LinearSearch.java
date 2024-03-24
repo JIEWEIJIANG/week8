4b6f2c82573ee576e08d5da56fb0f941a04ea331
JIEWEIJIANG <157995032+JIEWEIJIANG@users.noreply.github.com>
2024-03-24 14:12:58 -0400

    Update LinearSearch.java

--- src/main/java/com/mycompany/week8_skeletoncode_lab/LinearSearch.java
+++ src/main/java/com/mycompany/week8_skeletoncode_lab/LinearSearch.java
@@ -15,6 +15,16 @@ public class LinearSearch {
         int n = arr.length;
         // Todo 01: - complete the implementation of linear search and test your code  
          //         - prvoide asymptotic analysis of the developed solution
+         //iterate through each element
+        for(int i =0; i < n; i++){
+            //if the element is equal to target val, return its index
+            if(arr[i] == x){
+                return i;
+            }
+        }
+        //if the target val is not found, return -1
+        return -1;
     }
     
 }
+
