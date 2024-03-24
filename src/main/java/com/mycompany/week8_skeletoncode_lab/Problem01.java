3140b64a57b0e043d2bff87f398a3fae82d8f5c9
JIEWEIJIANG <157995032+JIEWEIJIANG@users.noreply.github.com>
2024-03-24 14:13:33 -0400

    Update Problem01.java

--- src/main/java/com/mycompany/week8_skeletoncode_lab/Problem01.java
+++ src/main/java/com/mycompany/week8_skeletoncode_lab/Problem01.java
@@ -15,6 +15,24 @@ public class Problem01 {
     //          Test your solution
     //          Analyze its space and time  
     
+    long sum = 0;
+        //iterate through each number from 2 to n
+        for (int num = 2; num <= n; num++) {
+            boolean isPrime = true;
+
+            //check if the num is prime
+            for (int i = 2; i <= Math.sqrt(num); i++) {
+                if (num % i == 0) {
+                    isPrime = false;
+                    break;
+                }
+            }
+            //if nnum is prim, add it to the sum
+            if (isPrime) {
+                sum += num;
+            }
+        }
+        return sum;
+
     }
-    
 }
